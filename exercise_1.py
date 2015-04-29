70d23ebe85f40043393008b597c01d4b5ca6c549
unknown <jyjung@jyjung-n.humaxdigital.com>
2015-04-29 17:42:31 +0900

    Initial commit

---
+++ exercise_1.py
@@ -0,0 +1,17 @@
+# _*_ coding utf-8 _*_
+
+__author__ = 'jyjung'
+
+tuple = (4, 5)
+list = [6, 7]
+
+x, y = tuple
+dict = {
+    'name' : '정지영',
+    '직책' : '대리'
+}
+
+print p
+print x
+print y
+print dict
